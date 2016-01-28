9a468093a92f32db08ff050778b06a92016cb9dc
Restym <pv.halfweg@hotmail.com>
2016-01-28 11:23:38 +0100

    Account en Manage verwijderd en gezorgd dat Restaurants fixt

--- IHFF/Controllers/HomeController.cs
+++ IHFF/Controllers/HomeController.cs
@@ -26,22 +26,6 @@ namespace IHFF.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
-        }
-
-        [HttpPost]
-        public ActionResult RetrieveWishlist(WishList code)
-        {
-            //Het opvragen van een eerder in de database opgeslagen WishList.
-            if (ModelState.IsValid)
-            {
-                // WishList retrieveWishlist = **database connection.
-                // If (WishlistCode == DatabaseCode)
-                // { redirect to WishList page with all items}
-                // Else 
-                // { Message: Code incorrect probeer opnieuw }
-                return View(code);
-            }
-            return RedirectToAction("~/Bestelling/...cshtml");
-        }
+        }       
     }
 }
